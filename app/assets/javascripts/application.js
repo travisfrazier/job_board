46e9f3213e4528f9f7dd5df5725dcbb397c0e041
travisfrazier <travisf777@gmail.com>
2017-12-13 13:32:02 -0500

    new

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,13 +17,68 @@
 //= require jquery_ujs
 //= require home
 
-$(document).ready(function() {
-  
-      $('#table tr').click(function() {
-          var href = $(this).find("a").attr("href");
-          if(href) {
-              window.location = href;
-          }
-      });
-  
+/* ----------------------------------
+	@ Jolpai
+	@ Version: 1.0
+	@ Author: DynamicWebLab
+	@Email: dynamicweblab@gmail.com
+-------------------------------------*/
+"use strict";
+//Make sure jQuery has been loaded before main.js
+if (typeof jQuery === "undefined") {
+  throw new Error("Jolpai requires jQuery to work on!");
+}
+
+/* jolpaiObj
+ *
+ * @type Object
+ * @description $.jolpaiObj Will hold options and functions.
+ */
+$.jolpaiObj = {};
+
+/* ---------------------
+ * /// Jolpai Options ///
+ * ---------------------
+ * You can enable disable functions here
+ */
+$.jolpaiObj.options = {
+
+};
+
+
+/* topMenu()
+ * ==========
+ * Adds Menu functionality.
+ *
+ * @type Function
+ * @usage: $.jolpaiObj.topMenu()
+ */
+$.jolpaiObj.TopMenu = function() {
+
+  var navMenu = $('.menu-list')
+
+  navMenu.onePageNav();
+
+  $(window).on('scroll resize', function(e) {
+    if ($(this).scrollTop() > 70) {
+      $('.header-section').addClass('sticky animated fadeInDown');
+    } else {
+      $('.header-section').removeClass('sticky animated fadeInDown');
+    }
+    e.preventDefault();
   });
+
+  $('.responsive').on('click', function(event) {
+    $('.menu-list').slideToggle(400);
+    $('.header-section').toggleClass('bgc');
+    event.preventDefault();
+  });
+
+  $('.menu-list li a').on('click', function(event) {
+    if ($(window).width() < 768) {
+      $('.menu-list').slideUp(400);
+      $('.header-section').removeClass('bgc');
+    }
+  });
+
+};
